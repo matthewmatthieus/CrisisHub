4a5c85df0b3642f7281c7e1ced719464d795809a
matthewmatthieus <25050771@myrp.edu.sg>
2026-07-23 10:26:18 +0800

    Allows incidents dashboard to be live

--- routes/verification.js
+++ routes/verification.js
@@ -2,7 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const verificationController = require("../controllers/verificationController");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
-router.get("/", verificationController.index);
+router.get("/", isAuthenticated, verificationController.index);
+
+router.post("/:id/confirm",
+    isAuthenticated,
+    verificationController.confirmIncident
+);
+
+router.post("/:id/dispute",
+    isAuthenticated,
+    verificationController.disputeIncident
+);
 
 module.exports = router;
